15ab6557d03321ee8e0dd36aa7db74ef4639df0b
pimchanok <janecussai25@gmail.com>
2018-01-12 16:06:17 +0700

    Finish 12Jan

--- app/src/main/java/com/example/lab3201_00/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lab3201_00/myapplication/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.lab3201_00.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.lab3201_00.myapplication.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment to Activity
+        if (savedInstanceState==null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+
+
+    } //Main Method
+
+} //Main Class
